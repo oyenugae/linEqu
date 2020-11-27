5a4530e2d0b545b3dce8133b7e1dd291ab5461f8
oyenugae <oyenugae@babcock.edu.ng>
2020-11-27 07:57:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    float first, second;
+    System.out.println("Input the value of a");
+    Scanner scan = new Scanner(System.in);
+    first = scan.nextFloat();
+    System.out.println("Input the value of b");
+    second = scan.nextFloat();
+    if (first == 0)
+       System.out.println("Do not make me have a division by zero errror by your zero input");
+    else
+       System.out.println("The answer to your linear equation is" + linEqu(first, second));
+  }
+
+  public static float linEqu(float a, float b){
+    return -b/a;
+  }
+}
